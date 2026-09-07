cdb214de75698f739932b330bdce13263f3c69ed
AndMarc-rpo251 <andrejmarcenuk40@gmail.com>
2026-09-07 13:59:17 +0300

    Update main.py

--- PythonProject1/main.py
+++ PythonProject1/main.py
@@ -1,8 +1,21 @@
-﻿"""ОСНОВНОЙ ФАЙЛ ПРИЛОЖЕНИЯ Task Manager
+"""ОСНОВНОЙ ФАЙЛ ПРИЛОЖЕНИЯ Task Manager
    version 0.0.1
-  -[] создать основной цикл задач"""
+  -[x] создать основной цикл задач
+  -[x] сделать функцию - показать заметки
+  -[x] сделать функцию - добавить заметки"""
+from random import choice
+
+collection = [] #list
 is_start = True #flag
-    print('start')
-    while(False):
-is_start =False
-    print('finish')
+
+while (is_start):
+    print('1 - показать задачи | 2 - добавить заметки')
+    choice_user = input('Введите ваш выбор (1 или 2)')
+    match choice_user:
+        case '1':
+            print(collection)
+        case '2':
+            collection.append('task')
+            print(collection)
+        case _:
+            print('Такого пункта нет')
