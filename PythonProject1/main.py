630a94aec6e1bac24d59e2df34c2376daa9bc859
AndMarc-rpo251 <andrejmarcenuk40@gmail.com>
2026-09-07 09:32:22 +0300

    Update main.py

--- PythonProject1/main.py
+++ PythonProject1/main.py
@@ -1,1 +1,8 @@
-﻿print('RPO-251')
+﻿"""ОСНОВНОЙ ФАЙЛ ПРИЛОЖЕНИЯ Task Manager
+   version 0.0.1
+  -[] создать основной цикл задач"""
+is_start = True #flag
+    print('start')
+    while(False):
+is_start =False
+    print('finish')
