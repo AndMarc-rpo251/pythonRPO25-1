d235aca39b8a01d1d9b3fb66f05088e35ee919b3
AndMarc-rpo251 <andrejmarcenuk40@gmail.com>
2026-09-08 09:24:34 +0300

    Update main.py

--- PythonProject1/main.py
+++ PythonProject1/main.py
@@ -3,19 +3,28 @@
   -[x] создать основной цикл задач
   -[x] сделать функцию - показать заметки
   -[x] сделать функцию - добавить заметки"""
-from random import choice
 
 collection = [] #list
 is_start = True #flag
 
 while (is_start):
-    print('1 - показать задачи | 2 - добавить заметки')
-    choice_user = input('Введите ваш выбор (1 или 2)')
+    print('1 - показать задачи | 2 - добавить заметки | 3 - удалить заметку')
+    choice_user = input('Введите ваш выбор (1, 2, 3)')
     match choice_user:
         case '1':
             print(collection)
         case '2':
-            collection.append('task')
+            collection.append(input())
             print(collection)
+        case '3':
+            print("выберите заметку", collection)
+            a = input()
+            if a in collection:
+                collection.remove(a)
+                print('заметка удалена')
+            else:
+                print('Такой заметки нет')
+            print(collection)
+        
         case _:
             print('Такого пункта нет')
