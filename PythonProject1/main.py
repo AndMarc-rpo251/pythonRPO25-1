20ca6fa8f78586926b7735a4d838cf8969532791
AndMarc-rpo251 <andrejmarcenuk40@gmail.com>
2026-09-14 09:22:52 +0300

    Update main.py

--- PythonProject1/main.py
+++ PythonProject1/main.py
@@ -1,21 +1,27 @@
 """ОСНОВНОЙ ФАЙЛ ПРИЛОЖЕНИЯ Task Manager
-   version 0.0.1
-  -[x] создать основной цикл задач
-  -[x] сделать функцию - показать заметки
-  -[x] сделать функцию - добавить заметки"""
+   version 0.0.4
+--- description ---
+приложение может сохранять, редактировать, удалять задачи
+"""
 
 collection = [] #list
-is_start = True #flag
+is_running = True #flag
 
-while (is_start):
-    print('1 - показать задачи | 2 - добавить заметки | 3 - удалить заметку')
-    choice_user = input('Введите ваш выбор (1, 2, 3)')
-    match choice_user:
+
+def show_collection(task_collection):
+    for i, j in enumerate(task_collection):
+        print(i + 1, j)
+    print("`~"*30)
+
+while (is_running):
+    print('1 - показать задачи | 2 - добавить заметки | 3 - удалить заметку | 4 - редактировать заметку | 0 - выход')
+    choice_user = input('Введите ваш выбор (1, 2, 3, 4, 0)')
+    match str(choice_user):
         case '1':
-            print(collection)
+            show_collection(collection)
         case '2':
             collection.append(input())
-            print(collection)
+            show_collection(collection)
         case '3':
             print("выберите заметку", collection)
             a = input()
@@ -24,7 +30,16 @@ while (is_start):
                 print('заметка удалена')
             else:
                 print('Такой заметки нет')
-            print(collection)
-        
+            show_collection()
+        case '4':
+            for i, j in enumerate(collection):
+                print(i + 1, j)
+            select_task = int(input('Введите номер задачи для редактирования'))
+            edit_task = input('Введите новое название')
+            collection[select_task - 1] = edit_task
+            show_collection(collection)
+        case '0':
+            is_running = False
         case _:
             print('Такого пункта нет')
+            show_collection(collection)
